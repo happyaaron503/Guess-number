5a65a6ee3c31f5857bb0e508e4efa749175e6176
happyaaron503 <blackknight1251@gmail.com>
2023-12-19 18:48:06 +0800

    add guessing times

--- guess_number.py
+++ guess_number.py
@@ -1,12 +1,16 @@
 import random
 r = random.randint(1, 10)
+count = 0
 while True:
     gus = int(input("Guess a number: "))
+    count += 1
     if gus == r:
         print("Ur right!")
+        print("猜了", count, "次")
         break
     else:
         print("Ur wrong! Keep guessing")
+        print("猜了", count, "次")
         if r > gus:
             print("Too small")
         elif r < gus:
